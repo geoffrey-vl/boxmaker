b8f0681c7ebbefc0c0dbcd823ff06a1b58f129e8
applemuncy <j.apple.muncy@gmail.com>
2016-08-23 13:42:06 -0400

    Adding simple install.bat for M$ Windows users.

---
+++ install.bat
@@ -0,0 +1,6 @@
+copy ink_helper.py C:\Users\%USERNAME%\AppData\Roaming\inkscape\extensions\ink_helper.py
+copy my_boxmaker.py C:\Users\%USERNAME%\AppData\Roaming\inkscape\extensions\my_boxmaker.py
+copy my_box.py C:\Users\%USERNAME%\AppData\Roaming\inkscape\extensions\my_box.py
+copy my_panel.py C:\Users\%USERNAME%\AppData\Roaming\inkscape\extensions\my_panel.py
+copy my_edge.py C:\Users\%USERNAME%\AppData\Roaming\inkscape\extensions\my_edge.py
+copy my_boxmaker.inx C:\Users\%USERNAME%\AppData\Roaming\inkscape\extensions\my_boxmaker.inx
